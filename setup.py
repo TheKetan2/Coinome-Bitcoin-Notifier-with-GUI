d1d3c7c74e1caf138c40c871d9a8509e38e8a7b1
Ketan Ramteke <Ketantoo@gmail.com>
2018-01-25 21:38:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=[''],
     url='github.com/TheKetan2',
     license='Free',
-    author='Ketan D. Ramteke',
+    author='Ketan Ramteke',
     author_email='',
     description='Bitcoin price notifier for Coinome bitcoin traders.'
 )
