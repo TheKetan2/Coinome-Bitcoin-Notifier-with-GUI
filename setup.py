7051e5634d699798b238b740d3bff6ea5cfb5e03
Ketan Ramteke <Ketantoo@gmail.com>
2018-03-21 21:30:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='coinomem_price_a lecmrmt',
+    name='coinomem_price_a lecm rmt',
     version='1.0',
     packages=[''],
     url='github.com/TheKetan2',
